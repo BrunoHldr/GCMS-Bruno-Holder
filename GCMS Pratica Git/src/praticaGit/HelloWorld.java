365e886924ca2979b16fa568d93d39c937885773
Bruno Holder <Bruno Holder@DESKTOP-U6GP3RS>
2022-06-01 21:18:53 -0300

    Gitando

---
+++ GCMS Pratica Git/src/praticaGit/HelloWorld.java
@@ -0,0 +1,13 @@
+package praticaGit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Agora vai !!!");
+		System.out.println("Agora vai 2!!!");
+		System.out.println("Agora vai 3!!!");
+
+	}
+
+}
